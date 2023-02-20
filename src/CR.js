37da2810531465466057da00696aed1638bea54c
bob <108271839+minegamer72@users.noreply.github.com>
2023-02-20 10:54:07 -0500

    removed some formatting and add comments

--- src/CR.js
+++ src/CR.js
@@ -1,15 +1,21 @@
-        /* small easter egg */
-        var today = new Date();
-        var dd = String(today.getDate()).padStart(2, '0');
-        var mm = String(today.getMonth() + 1).padStart(2, '0');
-        var main = document.getElementById("main").innerHTML;
+/* small easter egg i thought was cool. CR stands for Code Red, explained later in the code. */
 
-        today = mm + '/' + dd;
+/* build the current date like legos because JS is too cool to not do this automatically */
+var today = new Date();
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth() + 1).padStart(2, '0');
+var main = document.getElementById("main").innerHTML;
 
-        if(today === "07/15"){
-            document.body.innerHTML = '<html><head><title>HELLO!</title></head><body><hr size=5><font color="red"><p align="center">Welcome to http://www.worm.com !<br><br>Hacked By Chinese!</font></hr></body><br><br><br><br><br><br><br><br><br><button onclick="restore()">Restore</button> </html>';
+/* concatenate month and day */
+today = mm + '/' + dd;
 
+/* if what was created is 7/15 or July 15th */
+if(today === "07/15"){
+        /* replace all website content with the HTML found in the code red virus (awesome video by danooct1 shows what it does: https://youtu.be/iu48QBJP_p0 ) */
+        /* also adds a restore button to allow the user to go back to regularly scheduled bob. TODO: make a cookie that will stop this after the first time. */
+        document.body.innerHTML = '<html><head><title>HELLO!</title></head><body><hr size=5><font color="red"><p align="center">Welcome to http://www.worm.com !<br><br>Hacked By Chinese!</font></hr></body><br><br><br><br><br><br><br><br><br><button onclick="restore()">Restore</button> </html>';
+        /* function restore restores the page from the div variable named "main". it's fired when the button is clicked. */
         function restore(){
         document.body.innerHTML = main;
         }
-    }
+}
