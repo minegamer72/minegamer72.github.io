fecc5643ec4b9c7f115f70342af43d2ca120982d
codwarrior <108271839+codwarrior@users.noreply.github.com>
2023-03-12 13:02:21 -0400

    remove bad comments

--- src/getcat.js
+++ src/getcat.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-    // Make a request to the API to get a list of 3 random cat images
-    $.get('https://api.thecatapi.com/v1/images/search?limit=3', function(data) {
-        // Loop through the first 3 images and add them to the page
+    // make a request to the API to get a list of 1 random cat images
+    $.get('https://api.thecatapi.com/v1/images/search?limit=1', function(data) {
+        // loop through the first 3 images and add
         $.each(data.slice(0, 1), function(index, image) { // this could be different but i'm referencing previous code and i'm lazy soooo
             const imgtag = '<img src="' + image.url + '" '
             const imgtag2 = 'alt="Theres supposed to be a cat here." class="cat">'
